3127b6a5d7283fc1bdbb700eac1e40582b418729
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-02-27 22:41:34 +0100

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const photoPalette = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    img => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+            data-source="${img.original}"
+             alt="${img.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join('');
+
+photoPalette.insertAdjacentHTML('afterbegin', markup);
+
 console.log(galleryItems);
+
+photoPalette.addEventListener('click', selectPhoto);
+
+function selectPhoto(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: '205',
+  });
+}
